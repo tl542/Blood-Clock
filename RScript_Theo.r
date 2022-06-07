81fd8166c72aa1714974181204b6353294ae73db
tl542 <tl542@exeter.ac.uk>
2022-06-07 12:37:45 +0100

    Delete Training plot

--- RScript_Theo.r
+++ RScript_Theo.r
@@ -65,67 +65,8 @@ t.test(train1$Age ~ train1$Sex, mu=0, alt="two.sided", paired=FALSE, var.eq=FALS
 t.test(new_df1$Age ~ new_df1$Sex, mu=0, alt="two.sided", paired=FALSE, var.eq=FALSE)
 
 
-# Get rid of men older than 65 ? To reduce further the Sex/Age bias
-table(new_df1$Sex == "Male", new_df1$Age)
-table(train1$Sex == "Male", train1$Age)
  
 
-
-# Restrict the original dataset down to samples aged less than 65 (868)
-new_df2 <- new_df[new_df$Age >= 40 & new_df$Age <= 65,]
-# Train/Test split on the restricted data
-# Training size: 608 and Test Size: 260
-set.seed(123)
-n <- nrow(new_df2)
-trainIndex <- sample(1:n, size=round(0.7*n), replace=FALSE)
-train2 <- new_df2[trainIndex,]
-test2 <- new_df2[-trainIndex,]
-dim(train2)
-dim(test2)
-
-
-# Two-sample t-test on Training data from restricted set
-t.test(train2$Age ~ train2$Sex, mu=0, alt="two.sided", paired=FALSE, var.eq=FALSE)
-
-# Two-sample t-test on restricted set
-t.test(new_df2$Age ~ new_df2$Sex, mu=0, alt="two.sided", paired=FALSE, var.eq=FALSE)
-
-
-# Use 3-fold cross validation to estimate the "best" lambda parameter
-# i.e. that gives lowest mean MSE
-library(glmnet)
-train2 <- train2[,-809499]
-test2 <- test2[,-809499]
-alpha <- 0.5
-cv_fit_train <- cv.glmnet(as.matrix(train2), train2$Age, nfolds=3, alpha=alpha, family="gaussian")  
-best_lambda <- cv_fit_train$lambda.min
-
-
-# Extract all the necessary probes for probes clock
-#l_probes <- list()
-#l_rmse <- list()
-#l_cor <- list()
-#corr = 1
-#RMSE = 0
-#while (RMSE < 3.6 & corr > 0.6){
-    #fit_train <- glmnet(as.matrix(train2), train2$Age, alpha=0.5, nlambda=10)
-    #pred_test <- predict(fit_train, as.matrix(test2),s=best_lambda)
-    #RMSE <- rmse(test$Age, pred_test)
-    #corr <- cor(test$Age, pred_test)
-    #if (RMSE > 3.6 | corr < 0.6){
-        #stop = TRUE
-        #break
-     }
-    #if (stop){break}
-    #coefs <- coef(fit_train, s=best_lambda)
-    #coefs_nz <- coefs[which(coefs != 0),]
-    #coefs_nz_df <- as.data.frame(coefs_nz)
-    #l <- as.data.frame(cbind(l, list(row_names(coefs_nz_df)[2:(nrow(coefs_nz_df)-1)])))
-    #l_rmse <- as.data.frame(rbind(l_rmse,RMSE))
-    #l_corr <- as.data.frame(rbind(l_cor, corr))
-    #ix <- which(colnames(train1) %in% rownames(coefs_nz_df)[2:(nrow(coefs_nz_df)-1)])
-    #train2 <- train2[,-ix]
-     
-#} 
+ 
 
 
