93eb126b33ca1e75690d842f3859b25d1b77f77a
tl542 <tl542@exeter.ac.uk>
2022-06-09 14:40:54 +0100

    Update Prediction script

--- RScript_Theo_Prediction.r
+++ RScript_Theo_Prediction.r
@@ -18,37 +18,40 @@ best_lambda <- cv_fit_train$lambda.min
 # Extract all the necessary probes for probes clock
 library(Metrics)
 l_probes <- list()
+l_probes_coef <- list()
 l_rmse <- list()
 l_cor <- list()
 corr <-  1
 RMSE <- 0
 stop <- FALSE
 n <- 0
-while (RMSE < 3.6 & corr > 0.6){
+while (RMSE < 4.5  & corr > 0.6){
+    n <- n + 1 
     fit_train <- glmnet(as.matrix(train1[,-ncol(train1)]), train1$Age, alpha=0.5, nlambda=10)
     pred_test <- predict(fit_train, as.matrix(test1[,-ncol(test1)]),s=best_lambda)
     RMSE <- rmse(test1$Age, pred_test)
     corr <- cor(test1$Age, pred_test)
-    if (RMSE > 3.6 | corr < 0.6){
-        stop = TRUE
-        break
-    }
-    if (stop){break}
-    n <- n + 1 
-    plot(test1$Age, pred_test, xlab="Chronological Age (years)", ylab="Predicted Age (years)", main = paste("Iteration", n)) 
+    l_rmse <- c(l_rmse,RMSE)
+    l_corr <- c(l_cor, corr)
+    plot(test1$Age, pred_test, xlab="Chronological Age (years)", ylab="Predicted Age (years)", main = paste("Model", n))
     text(45,75, paste("RMSE =", round(RMSE,2)))
-    text(45,70, paste("r =", round(corr,2)))     
-    dev.copy(pdf, "plot_clock.pdf")
+    text(45,70, paste("r =", round(corr,2)))
+    dev.copy(pdf, "plots_clock.pdf")
     dev.off()
     coefs <- coef(fit_train, s=best_lambda)
     coefs_nz <- coefs[which(coefs != 0),]
     coefs_nz_df <- as.data.frame(coefs_nz)
     l_probes <- c(l_probes, list(rownames(coefs_nz_df)[2:nrow(coefs_nz_df)]))
-    l_rmse <- c(l_rmse,RMSE) 
-    l_corr <- c(l_cor, corr)
+    l_probes_coef <- c(l_probes_coef, list(coefs_nz_df[,"coefs_nz"]))
     ix <- which(colnames(train1) %in% rownames(coefs_nz_df)[2:nrow(coefs_nz_df)])
     train1 <- train1[,-ix]
     test1 <- test1[,-ix]
+    if (RMSE > 4.5 | corr < 0.5){
+        stop = TRUE
+        break
+    }
+    if (stop){break}
     
 }
+
 
