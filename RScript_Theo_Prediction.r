e3f766fa0e0dc0f6b1ecc9910005b0b28431f5af
tl542 <tl542@exeter.ac.uk>
2022-06-09 20:48:07 +0100

    Update prediction script

--- RScript_Theo_Prediction.r
+++ RScript_Theo_Prediction.r
@@ -34,10 +34,11 @@ while (RMSE < 4.5 & corr > 0.6){
     corr <- cor(test1$Age, pred_test)
     l_rmse <- c(l_rmse,RMSE)
     l_cor <- c(l_cor, corr)
-    plt <- plot(test1$Age, pred_test, xlab="Chronological Age (years)", ylab="Predicted Age (years)", main = paste("Model", n))
+    plot(test1$Age, pred_test, xlab="Chronological Age (years)", ylab="Predicted Age (years)", main = paste("Model", n))
     text(45,75, paste("RMSE =", round(RMSE,2)))
     text(45,70, paste("r =", round(corr,2)))
-    ggsave(file='plot-cb.pdf', plt)    
+    dev.copy(pdf, "plot-cb.pdf")    
+    dev.off()     
     coefs <- coef(fit_train, s=best_lambda)
     coefs_nz <- coefs[which(coefs != 0),]
     coefs_nz_df <- as.data.frame(coefs_nz)
