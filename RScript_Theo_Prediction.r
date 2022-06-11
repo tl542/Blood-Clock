26aaf3dbf9b4e30ab48a2c2a0a2b660845f74bfd
tl542 <95082666+tl542@users.noreply.github.com>
2022-06-11 16:14:15 +0100

    Update RScript_Theo_Prediction.r

--- RScript_Theo_Prediction.r
+++ RScript_Theo_Prediction.r
@@ -15,7 +15,9 @@ cv_fit_train <- cv.glmnet(as.matrix(train1[,-ncol(train1)]), train1$Age, nfolds=
 best_lambda <- cv_fit_train$lambda.min
 
 
-# Extract all the necessary probes for probes clock
+# Store the correlation between DNAm age and actual on test data
+# While cor >= 6 
+# Save the selected probes with their weights
 library(Metrics)
 l_probes <- list()
 l_probes_coef <- list()
@@ -54,21 +56,54 @@ while (corr >= 0.6){
     
 }
 
+
+# Plot correlation change across the models making through the loop (n=105)
 l_cor_unlist <- unlist(l_cor)
 n_models <- unlist(list(1:n))
-plot(n_models, l_cor_unlist, pch=19, xlab="Model", ylab="Correlation")
-text(n_models, l_cor_unlist, paste("", round(l_cor_unlist,2)), pos=1)
-dev.copy(pdf, "cor_plot.pdf")
+plot(n_models, l_cor_unlist, pch=19, xlab="Model (n=105)", ylab="Correlation")
+#text(n_models, l_cor_unlist, paste("", round(l_cor_unlist,2)), pos=1)
+dev.copy(pdf, "cor_per_model(plot).pdf")
 dev.off()
 
+# Plot number of weighted probes across the models making through the loop (n=105)
 len_probes <- list()
 for (i in 1:length(l_probes)){
     len_probes <- c(len_probes, length(l_probes[[i]]))
 }
 len_probes_unlist <- unlist(len_probes)
-plot(n_models, len_probes_unlist, pch=19, xlab="Model", ylab="Number of Probes")
-text(n_models, len_probes_unlist, paste("", len_probes_unlist), pos=1)
-dev.copy(pdf, "probes_plot.pdf")
+plot(n_models, len_probes_unlist, pch=19, xlab="Model (n=105)", ylab="Number of Probes")
+#text(n_models, len_probes_unlist, paste("", len_probes_unlist), pos=1)
+dev.copy(pdf, "nb_probes_per_model(plot).pdf")
 dev.off()
+
+
+# Save correlation + number of probes for each of the 105 models (with cor >= 0.6 on test data)
+l_cor_df <- as.data.frame(l_cor_unlist)
+colnames(l_cor_df) <- "Correlation"
+len_probes_df <- as.data.frame(len_probes_unlist)
+colnames(len_probes_df) <- "nProbes"
+l_cor_nprobes_df <- cbind(len_probes_df, l_cor_df)
+for (i in 1:nrow(l_cor_nprobes_df)){
+    rownames(l_cor_nprobes_df)[i] <- paste("Model", i)
+}
+write.table(l_cor_nprobes_df, "nprobes+cor_per_model.txt", row.names=T, col.names=T, quote=F)
+
+
+
+
+#Save all the probes with their respective beta coefficient
+l_probes_coef_unlist <- unlist(l_probes_coef)
+l_probes_coef_unlist_df <- as.data.frame(l_probes_coef_unlist)
+colnames(l_probes_coef_unlist_df) <- "Coef"
+l_probes_unlist <- unlist(l_probes)
+l_probes_unlist_df <- as.data.frame(l_probes_unlist)
+colnames(l_probes_unlist_df) <- "Probes"
+
+df_new <- [l_probes_coef_unlist_df$coef <= 30,]
+df_new <- as.data.frame(df_new)
+df_final <- cbind(l_probes_unlist_df, df_new)
+write.table(df_final, "probes_coef.txt", row.names=T, col.names=T, quote=F)
+
+
 
 
