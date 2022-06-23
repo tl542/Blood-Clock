26041d507010064075367273a478ba64721caac1
tl542 <95082666+tl542@users.noreply.github.com>
2022-06-23 15:38:49 +0100

    update

--- RScript_EXTEND+US.r
+++ RScript_EXTEND+US.r
@@ -154,4 +154,4 @@ colnames(l_rmse_df) <- "RMSE"
 for (i in 1:nrow(l_rmse_df)){
     rownames(l_rmse_df)[i] <- paste("Model", i)
 }
-
+write.table(l_rmse_df, "RMSE (Split 1).txt", row.names=T, col.names=T, quote=F)
