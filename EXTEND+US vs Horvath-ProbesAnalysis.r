03c09ec5fbd6b5e7e7960c0253f28500318813fb
tl542 <95082666+tl542@users.noreply.github.com>
2022-07-07 14:10:39 +0100

    update

--- EXTEND+US vs Horvath-ProbesAnalysis.r
+++ EXTEND+US vs Horvath-ProbesAnalysis.r
@@ -219,7 +219,7 @@ horvath_boxplot[21:25,"Overlap_Proportion"] <- prop_probes_summary$Overlap_0.6_H
 horvath_boxplot[21:25,"Model_Criteria"] <- "Models_0.6"
 
 
-boxplot(horvath_boxplot$Overlap_Proportion ~ horvath_boxplot$Model_Criteria, xlab="Model_Criteria", ylab="Proportion of probe overlap", col="steelblue")
+boxplot(horvath_boxplot$Overlap_Proportion ~ horvath_boxplot$Model_Criteria, xlab="Model_Criteria", ylab="Proportion of probe overlap", col="steelblue", main="Proportion of Horvath clock probes overlapping with study probes")
 dev.copy("boxplot_horvath", pdf)
 dev.off()
 
