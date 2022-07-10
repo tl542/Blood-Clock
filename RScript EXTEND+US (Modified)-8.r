de05ead9b6f45a7ebd7a083268b292a1b87ee8fd
tl542 <95082666+tl542@users.noreply.github.com>
2022-07-10 13:18:39 +0100

    update

--- RScript EXTEND+US (Modified)-8.r
+++ RScript EXTEND+US (Modified)-8.r
@@ -1,3 +1,6 @@
+#!/usr/bin/env Rscript
+
+
 # Load in the RData
 data <- load("/mnt/data1/EXTEND/Methylation/QC/EXTEND_batch1_2_merged/EXTEND_batches_1_2_normalised_together.rdat")
 
@@ -10,8 +13,9 @@ pheno_df <- as.data.frame(pheno)
 
 
 # Combine DNAm df with phenotypic Sex variable
-new_df <- cbind(DNAm_df, pheno_df[,"Age"])
-colnames(new_df)[809499] <- "Age"
+new_df <- cbind(DNAm_df, pheno_df[,c("Sex","Age")])
+colnames(new_df)[809499] <- "Sex"
+colnames(new_df)[809500] <- "Age"
 
 
 
@@ -27,11 +31,11 @@ load("/mnt/data1/EPICQC/UnderstandingSociety/US_Betas_Pheno.rda")
 DNAm_us <- t(dat)
 DNAm_df_us <- as.data.frame(DNAm_us)
 pheno_df_us <- as.data.frame(pheno)
-new_df_us <- cbind(DNAm_df_us, pheno_df_us[,"confage"])
-colnames(new_df_us)[857131] <- "Age"
-new_df_us$Age <- as.integer(new_df_us$Age)
+new_df_us <- cbind(DNAm_df_us, pheno_df_us[,c("nsex","confage")])
+colnames(new_df_us)[857131] <- "Sex"
+colnames(new_df_us)[857132] <- "Age"
 new_df2_us <- new_df_us[new_df_us$Age >= 40 & new_df_us$Age <= 70,]
-
+new_df2_us$Age <- as.integer(new_df2_us$Age)
 
 
 
@@ -46,8 +50,8 @@ dim(df_all)
 set.seed(96)
 n <- nrow(df_all)
 trainIndex <- sample(1:n, size=round(0.7*n), replace=FALSE)
-train <- df_all[trainIndex,]
-test <- df_all[-trainIndex,]
+train <- df_all[trainIndex,-803377]
+test <- df_all[-trainIndex,-803377]
 dim(train)
 dim(test)
 
@@ -100,3 +104,67 @@ while (corr >= 0.6){
     
 }
 
+
+
+# Save correlation + number of probes + cumulative number of probes for each of the models (with cor >= 0.6 on test data)
+l_cor_unlist <- unlist(l_cor)
+l_cor_df <- as.data.frame(l_cor_unlist)
+colnames(l_cor_df) <- "Correlation"
+len_probes <- list()
+for (i in 1:length(l_probes)){
+    len_probes <- c(len_probes, length(l_probes[[i]]))
+}
+len_probes_unlist <- unlist(len_probes)
+len_probes_df <- as.data.frame(len_probes_unlist)
+colnames(len_probes_df) <- "nProbes"
+l_cor_nprobes_df <- cbind(l_cor_df,len_probes_df)
+l_cor_nprobes_df$Cum_nProbes <- cumsum(l_cor_nprobes_df$nProbes)
+for (i in 1:nrow(l_cor_nprobes_df)){
+    rownames(l_cor_nprobes_df)[i] <- paste("Model", i)
+}
+write.table(l_cor_nprobes_df, "table (Split 1).txt", row.names=T, col.names=T, quote=F)
+
+
+
+# Plot (+Save) correlation against number of weighted probes
+pdf("Plot (Split 1).pdf")
+plot(l_cor_nprobes_df$Cum_nProbes, l_cor_nprobes_df$Correlation, pch=19, xlab="Cumulative number of Probes", ylab="Correlation", main="Correlation vs (Cumulative) number of probes")
+dev.off()
+
+
+#Save RMSE for each of the models (with cor >= 0.6 on test data)
+l_rmse_unlist <- unlist(l_rmse)
+l_rmse_df <- as.data.frame(l_rmse_unlist)
+colnames(l_rmse_df) <- "RMSE"
+for (i in 1:nrow(l_rmse_df)){
+    rownames(l_rmse_df)[i] <- paste("Model", i)
+}
+write.table(l_rmse_df, "RMSE (Split 1).txt", row.names=T, col.names=T, quote=F)
+
+
+#Save all the probes selected by each model
+probes_model <- data.frame()
+for (i in 1:length(l_probes)){
+    l_probes_i <- unlist(l_probes[[i]])
+    l_probes_i_df <- as.data.frame(l_probes_i)
+    colnames(l_probes_i_df) <- "Selected Probes"
+    l_probes_i_df["Probes_Model"] <- paste("Model", i)
+    probes_model <- rbind(probes_model, l_probes_i_df)
+}
+write.table(probes_model, "probes_model (Split 1).txt", row.names=T, col.names=T, quote=F)
+
+
+#Save all the probes with their respective beta coefficient
+for (i in 1:length(l_probes_coef)){
+    l_probes_coef[[i]] <- l_probes_coef[[i]][-1]
+}
+
+l_probes_coef_unlist <- unlist(l_probes_coef)
+l_probes_coef_unlist_df <- as.data.frame(l_probes_coef_unlist)
+colnames(l_probes_coef_unlist_df) <- "Coef"
+l_probes_unlist <- unlist(l_probes)
+l_probes_unlist_df <- as.data.frame(l_probes_unlist)
+colnames(l_probes_unlist_df) <- "Probes"
+
+df_final <- cbind(l_probes_unlist_df, l_probes_coef_unlist_df)
+write.table(df_final, "probes_coef (Split 1).txt", row.names=T, col.names=T, quote=F)
