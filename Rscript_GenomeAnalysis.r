31b1941d33456d5ed3a243af0da410b2ff1042a9
tl542 <95082666+tl542@users.noreply.github.com>
2022-07-13 15:59:20 +0100

    update

--- Rscript_GenomeAnalysis.r
+++ Rscript_GenomeAnalysis.r
@@ -4,11 +4,22 @@ source("/mnt/data1/EXTEND/Methylation/QC/Theo/script_forGA.r")
 
 epicManifest<-read.csv("/mnt/data1/EPIC_reference/MethylationEPIC_v-1-0_B2.csv", skip = 7)
 
+
+
+
 df_final <- data.frame()
 for (i in seq(1,22)){
 probes_i <- epicManifest[epicManifest$Chromosome_36 == i, c("Name","Chromosome_36")]
 df_final <- rbind(df_final, probes_i)
 }
+
+set.seed(84)
+n <- nrow(df_all)
+trainIndex <- sample(1:n, size=round(0.7*n), replace=FALSE)
+train <- df_all[trainIndex,]
+test <- df_all[-trainIndex,]
+best_lamdba <- 0.1355626
+
 
 library(Metrics)
 l_probes <- list()
@@ -20,20 +31,13 @@ for (i in 1:22){
   n_iter <- n_iter + 1
   probes_i <- df_final[df_final$Chromosome_36 == i,]
   rownames(probes_i) <- probes_i$Name
-  ix <- which(colnames(df_all)[-803377] %in% rownames(probes_i))
-  new_df <- cbind(df_all[,ix], df_all$Age)
-  names(new_df)[names(new_df) == "df_all$Age"] <- "Age"
-  set.seed(i)
-  n <- nrow(new_df)
-  trainIndex <- sample(1:n, size=round(0.7*n), replace=FALSE)
-  train <- new_df[trainIndex,]
-  test <- new_df[-trainIndex,]
-  library(glmnet)
-  alpha <- 0.5
-  cv_fit_train <- cv.glmnet(as.matrix(train[,-ncol(train)]), train$Age, nfolds=10, alpha=alpha, family="gaussian")
-  best_lambda <- cv_fit_train$lambda.min
-  fit_train <- glmnet(as.matrix(train[,-ncol(train)]), train$Age, alpha=0.5, nlambda=10)
-  pred_test <- predict(fit_train, as.matrix(test[,-ncol(test)]), s=best_lambda)
+  ix <- which(colnames(train)[-803377] %in% rownames(probes_i))
+  new_train <- cbind(train[,ix], train$Age)
+  new_test <- cbind(test[,ix], test$Age)
+  names(new_train)[names(new_train) == "train$Age"] <- "Age"
+  names(new_test)[names(new_test) == "test$Age"] <- "Age"
+  fit_train <- glmnet(as.matrix(new_train[,-ncol(new_train)]), new_train$Age, alpha=0.5, nlambda=10)
+  pred_test <- predict(fit_train, as.matrix(new_test[,-ncol(new_test)]), s=best_lambda)
   RMSE <- rmse(test$Age, pred_test)
   corr <- cor(test$Age, pred_test)
   l_rmse <- c(l_rmse,RMSE)
@@ -42,7 +46,7 @@ for (i in 1:22){
   coefs_nz <- coefs[which(coefs != 0),]
   coefs_nz_df <- as.data.frame(coefs_nz)
   l_probes <- c(l_probes, list(rownames(coefs_nz_df)[2:nrow(coefs_nz_df)]))
-  l_probes_coef <- c(l_probes_coef, list(coefs_nz_df[,"coefs_nz"]))
+  l_probes_coef <- c(l_probes_coef, list(coefs_nz_df[-1,"coefs_nz"]))
  
 }
 
