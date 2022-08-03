fd48ecfeaadf7a515d1c1e05f56f21a9e7961896
tl542 <95082666+tl542@users.noreply.github.com>
2022-08-03 09:34:07 +0100

    uldate

--- RScript _Sliding Window.r
+++ RScript _Sliding Window.r
@@ -40,7 +40,6 @@ for (c in seq(1,22)){
   rownames(probes_chr_ordered) <- NULL
   v <- probes_chr_ordered$MAPINFO 
   for (i in v){
-    n_iter <- n_iter + 1
     l_p <- list()
     l_p <- which(v < i+500000 & v >= i) 
     start_window <- c(start_window, i)
@@ -48,19 +47,21 @@ for (c in seq(1,22)){
     probes_window <- probes_chr_ordered[l_p,]
     rownames(probes_window) <- probes_window$Name
     ix <- which(colnames(df_all)[-803377] %in% rownames(probes_window))
-    new_train <- train[,c(ix,ncol(train))]
-    new_test <- test[,c(ix,ncol(test))]
-    fit_train <- glmnet(as.matrix(new_train[,-ncol(new_train)]), new_train$Age, alpha=0.5, nlambda=10)
-    pred_test <- predict(fit_train, as.matrix(new_test[,-ncol(new_test)]), s=best_lambda)
-    RMSE <- rmse(new_test$Age, pred_test)
-    corr <- cor(new_test$Age, pred_test)
-    l_rmse <- c(l_rmse,RMSE)
-    l_cor <- c(l_cor, corr)
-    coefs <- coef(fit_train, s=best_lambda)
-    coefs_nz <- coefs[which(coefs != 0),]
-    coefs_nz_df <- as.data.frame(coefs_nz)
-    l_probes <- c(l_probes, list(rownames(coefs_nz_df)[2:nrow(coefs_nz_df)]))
-    l_probes_coef <- c(l_probes_coef, list(coefs_nz_df[-1,"coefs_nz"]))
+    if (length(ix) >= 2){
+      n_iter <- n_iter + 1
+      new_train <- train[,c(ix,ncol(train))]
+      new_test <- test[,c(ix,ncol(test))]
+      fit_train <- glmnet(as.matrix(new_train[,-ncol(new_train)]), new_train$Age, alpha=0.5, nlambda=10)
+      pred_test <- predict(fit_train, as.matrix(new_test[,-ncol(new_test)]), s=best_lambda)
+      RMSE <- rmse(new_test$Age, pred_test)
+      corr <- cor(new_test$Age, pred_test)
+      l_rmse <- c(l_rmse,RMSE)
+      l_cor <- c(l_cor, corr)
+      coefs <- coef(fit_train, s=best_lambda)
+      coefs_nz <- coefs[which(coefs != 0),]
+      coefs_nz_df <- as.data.frame(coefs_nz)
+      l_probes <- c(l_probes, list(rownames(coefs_nz_df)[2:nrow(coefs_nz_df)]))
+      l_probes_coef <- c(l_probes_coef, list(coefs_nz_df[-1,"coefs_nz"]))
   }
   nmodels_chr <- c(nmodels_chr, n_iter)
 }
